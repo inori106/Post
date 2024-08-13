790091013ab3371faaf6825db1aacb44bc406407
Inori <lijiateng34@gmail.com>
2024-08-14 01:24:54 +0900

    chore: update input field name in category edit page

--- src/app/category/edit/[id]/page.tsx
+++ src/app/category/edit/[id]/page.tsx
@@ -14,7 +14,7 @@ export default async function EditPage({ params }: { params: { id: string } }) {
           <label className='text-xl font-semibold'>カテゴリー名</label>
           <input
             type='text'
-            name='title'
+            name='name'
             defaultValue={category.name}
             className='py-3 mt-1 px-2 w-full rounded-md border-2 border-gray-700'
           />
